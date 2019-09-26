3916bb2c2450ca94f8636742983afc0c4b123ef9
Dalitita18 <elva.villanueva16@tectijuana.edu.mx>
2019-09-25 18:26:27 -0700

    practicas

--- Practicas/Practica4.scala
+++ Practicas/Practica4.scala
@@ -8,22 +8,39 @@ val df = spark.read.option("header", "true").option("inferSchema","true")csv("Ci
 //1
 df.show()
 //2
-df.
+df.filter($"Close" < 480 && $"High" < 480).show()
 //3
+df.select(corr("High", "Low")).show()
 //4
+df.columns
 //5
+df.count
 //6
+df.select(corr("High", "Low")).show()
 //7
+df.select(sum("High")).show()
 //8
+df.select(min("High")).show()
 //9
+df.select(max("High")).show()
 //10
+
 //11
+df.select(mean("High")).show()
 //12
+
 //13
+
 //14
+
 //15
+
 //16
+
 //17
+
 //18
+
 //19
+
 //20
