961a1339e6b07b22fb1097d49fa0fc8d3cd4c19d
Dalitita18 <elva.villanueva16@tectijuana.edu.mx>
2019-12-10 07:25:14 -0800

    calis

--- Unidad_1/Practicas/Practica1.scala
+++ Unidad_1/Practicas/Practica1.scala
@@ -1,14 +1,5 @@
-//
-// Practica 1
-//1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
-//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
-//3. Dada la variable bird = "tweet", utiliza interpolacion de string para
-//   imprimir "Estoy ecribiendo un tweet"
-//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
-//   secuencia "Luke"
-//5. Cual es la diferencia en value y una variable en scala?
-//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
 
+// Practica 1
 
 //1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
 val deametro =4.5
